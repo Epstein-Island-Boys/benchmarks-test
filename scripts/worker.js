779f7303b8017a08a5a49cc893bc631ee768b992
Epstein-Island-Boys <smurfcat21monday@gmail.com>
2026-04-21 20:30:03 -0400

    Update worker.js

--- scripts/worker.js
+++ scripts/worker.js
@@ -1,6 +1,9 @@
-// worker.js
-self.onmessage = function() {
-    setInterval(() => {
-        self.postMessage("pulse");
-    }, 50); 
+// scripts/worker.js
+self.onmessage = function(e) {
+    if (e.data === "start") {
+        setInterval(() => {
+            // Sends a signal back to the main script every 40ms
+            self.postMessage("tick");
+        }, 40); 
+    }
 };
