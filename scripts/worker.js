da8c48535a60d5c702110432d31b811dcc9597e8
Epstein-Island-Boys <smurfcat21monday@gmail.com>
2026-04-23 21:57:37 -0400

    Update worker.js

--- scripts/worker.js
+++ scripts/worker.js
@@ -2,17 +2,7 @@
 self.onmessage = function(e) {
     if (e.data === "start") {
         setInterval(() => {
-            // We fire the burst inside the worker
-            for (let b = 0; b < 25; b++) {
-                const salt = Math.random().toString(36).substring(2, 10);
-                const testUrl = `https://www.google.com/search?q=tiktok&v=${salt}`;
-
-                // Fetching inside a worker bypasses the Main Browser Thread
-                fetch(testUrl, { 
-                    mode: 'no-cors', 
-                    cache: 'no-store' 
-                }).catch(() => {});
-            }
-        }, 30); // 30ms pulse for high intensity
+            self.postMessage("pulse");
+        }, 15); // Faster pulse, but lighter work
     }
 };
